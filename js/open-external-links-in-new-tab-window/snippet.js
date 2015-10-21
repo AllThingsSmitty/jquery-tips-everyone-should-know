eff6b6af84aae66c6f298a4edebba3bb106edb96
Matt Smith <runner_28@hotmail.com>
2015-10-21 10:17:49 -0400

    Added snippet 😎

---
+++ js/open-external-links-in-new-tab-window/snippet.js
@@ -0,0 +1,4 @@
+// Open external links in new tab/window
+$('a[href^="http"]').attr('target','_blank');
+$('a[href^="//"]').attr('target','_blank');
+$('a[href^="'+window.location.origin+'"]').attr('target','_self');
