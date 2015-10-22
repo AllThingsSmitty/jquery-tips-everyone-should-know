c9ae5c4451e0d098bc95e2be23ed6045375f0a78
Matt Smith <runner_28@hotmail.com>
2015-10-22 11:42:38 -0400

    Updated snipped 👍🏼

--- js/make-two-divs-the-same-height/snippet.js
+++ js/make-two-divs-the-same-height/snippet.js
@@ -7,3 +7,9 @@ $columns.each(function () {
   }
 });
 $columns.height(height);
+
+// Make all child divs the same height
+var $rows = $('.same-height-columns');
+$rows.each(function () {
+  $(this).find('.column').height($(this).height());
+});
