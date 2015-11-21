1eb990e6bfe599d92749635d29b4a8536a168ffc
Matt Smith <runner_28@hotmail.com>
2015-11-21 08:50:36 -0500

    Linted syntax 🔧

---
+++ js/check-if-jquery-loaded/snippet.js
@@ -0,0 +1,6 @@
+// Check If jQuery Loaded
+if (typeof jQuery == undefined) {
+  // jQuery hasn't loaded
+} else {
+  // jQuery has loaded
+}
--- js/checking-if-jquery-loaded/snippet.js
+++
@@ -1,8 +0,0 @@
-// Checking If jQuery Loaded
-if (typeof jQuery == undefined) {
-    // jQuery isn't here
-    // Enter helpful alert() or console.log() message here
-}
-else {
-    // jQuery is here
-}
