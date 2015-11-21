c5d6bf62d780c71c84801a270c5b66cf75561b58
Matt Smith <runner_28@hotmail.com>
2015-11-21 09:51:26 -0500

    Linting 🔧

--- js/trigger-on-visibility-change/snippet.js
+++ js/trigger-on-visibility-change/snippet.js
@@ -1,8 +1,8 @@
 // Trigger on visibility change
 $(document).on('visibilitychange', function (e) {
-  if (e.target.visibilityState === "visible") {
+  if (e.target.visibilityState === 'visible') {
     console.log('Tab is now in view!');
-  } else if (e.target.visibilityState === "hidden") {
+  } else if (e.target.visibilityState === 'hidden') {
     console.log('Tab is now hidden!');
   }
 });
